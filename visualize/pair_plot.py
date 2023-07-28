2487a523a0ca45d8bcb2e2c27aa97ad489434958
Charles Mariot <contact@charles-mariot.fr>
2023-07-28 14:12:36 +0200

    Backup: Petits ajouts sur descibe, options sur pair_plot et verifs target

--- visualize/pair_plot.py
+++ visualize/pair_plot.py
@@ -1,7 +1,7 @@
 import seaborn as sns
 import pandas
 import argparse
-import matplotlib.pyplot as plt
+from matplotlib.pyplot import (savefig, show)
 
 
 def parse_arguments() -> tuple:
@@ -11,12 +11,42 @@ def parse_arguments() -> tuple:
     try:
         parser = argparse.ArgumentParser(
             prog="pair_plot",
-            description=""
+            description="This plot helps us to choose features" +
+                        " for logistic_regression."
+
         )
-        parser.add_argument('dataset_path')
+        parser.add_argument(
+            '-p', '--path',
+            dest="dataset_path",
+            type=str,
+            default="../datasets/dataset_train.csv",
+            help="Path to the dataset."
+        )
+        parser.add_argument(
+            '-t', '--target',
+            dest="target_name",
+            type=str,
+            default="Hogwarts House",
+            help="Target name."
+        )
+        parser.add_argument(
+            '-c', '--corner',
+            dest="corner",
+            action='store_true',
+            help="Plot only one part of the pair plot."
+        )
+        parser.add_argument(
+            '-s', '--save-png',
+            dest="save_png",
+            action='store_true',
+            help="Save the pair_plot in a png file."
+        )
         args = parser.parse_args()
         return (
-            args.dataset_path
+            args.dataset_path,
+            args.target_name,
+            args.corner,
+            args.save_png
         )
     except Exception as e:
         print("Error parsing arguments: ", e)
@@ -35,10 +65,14 @@ def read_dataset(dataset_path: str) -> pandas.DataFrame:
         exit()
 
 
-def select_columns(dataset: pandas.DataFrame) -> pandas.DataFrame:
+def drop_index(dataset: pandas.DataFrame) -> pandas.DataFrame:
     """
+    Remove the 'Index' column of the dataset.
     """
     try:
+        # Remove empty columns
+        dataset = dataset.dropna(axis=1, how='all')
+        # Remove 'Index' column
         without_index = dataset.drop("Index", axis='columns')
         return without_index
     except KeyError:
@@ -48,23 +82,46 @@ def select_columns(dataset: pandas.DataFrame) -> pandas.DataFrame:
         exit()
 
 
+def check_target(target_name, dataset) -> tuple:
+    """
+    Check if there is a 'target_name' column in the dataset.
+
+    """
+    try:
+        target = dataset[target_name]
+        # Check if objects ?
+        unique_targets = target.unique()
+        if len(unique_targets) == 4:
+            # Hogwarts houses colors
+            target_palette = {
+                unique_targets[0]: "#BC2F2F",  # Gryffindor
+                unique_targets[1]: "#8FBD61",  # Hufflepuff
+                unique_targets[2]: "#3D56D3",  # Ravenclaw
+                unique_targets[3]: "#E7E058"   # Slytherin
+            }
+        target_palette = None
+        return (unique_targets, target_palette)
+    except KeyError:
+        print(f"Error: Target {target_name} not found in the dataset.")
+        print("Consider using the --target option to specify a new target.")
+        exit()
+    except Exception as error:
+        print("Error:", error)
+        exit(1)
+
+
 if __name__ == "__main__":
 
-    dataset_path = parse_arguments()
+    (
+        dataset_path,
+        target_name,
+        corner,
+        save_png
+    ) = parse_arguments()
+
     entire_dataset = read_dataset(dataset_path)
-    dataset = select_columns(entire_dataset)
-
-    # Check if there is a Hogwarts House column
-    target = dataset["Hogwarts House"]
-    target_name = target.name
-    unique_targets = target.unique()
-
-    palette = {
-        "Gryffindor": "#BC2F2F",
-        "Hufflepuff": "#8FBD61",
-        "Ravenclaw": "#3D56D3",
-        "Slytherin": "#E7E058"
-    }
+    dataset = drop_index(entire_dataset)
+    (unique_targets, target_palette) = check_target(target_name, dataset)
 
     sns.set(font_scale=0.5)
 
@@ -72,14 +129,14 @@ if __name__ == "__main__":
         data=dataset,
         hue=target_name,
         hue_order=unique_targets,
-        palette=palette,
+        palette=target_palette,
         dropna=True,
         kind="scatter",
         diag_kind="hist",
         markers=".",
         height=1,  # inch
         aspect=2,  # width = height * aspect
-        corner=True,
+        corner=corner,
         plot_kws={
             "alpha": 0.5,
         },
@@ -88,9 +145,13 @@ if __name__ == "__main__":
         }
     )
 
-    pair_plot.fig.suptitle("Pair plot of numerical features")
+    # Figure title
+    pair_plot.fig.suptitle(
+        "Pair plot of numerical features"
+    )
 
     # Save the figure in a file, more readable than the figure
-    plt.savefig("pair_plot.png", dpi=400)
+    if save_png:
+        savefig("pair_plot.png", dpi=400)
 
-    plt.show()
+    show()
